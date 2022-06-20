c84a1c514b9f36bc76c363ce9d918f0f11c01288
Matt Fisher <matt.fisher@nsidc.org>
2022-06-20 22:56:14 +0000

    Better error handling

--- example_slackbots/hello_world.py
+++ example_slackbots/hello_world.py
@@ -3,23 +3,20 @@
 NOTE: Channel is configured in the Slack Application configuration, not in the bot
 itself.
 """
-from fastapi.responses import HTMLResponse
+from fastapi import HTTPException
 from slack_sdk.errors import SlackApiError
 
 from slackbotr.routers import slackbots_router
 from slackbotr.util.slack import web_client
 
 
-@slackbots_router.get('/test', response_class=HTMLResponse)
+@slackbots_router.get('/test')
 async def root() -> str:
-    # Based on: https://slack.dev/python-slack-sdk/web/index.html#messaging
-    # I don't really fully understand the error-handling portion.
-    # TODO: refactor?
     try:
         response = web_client.chat_postMessage(
             channel="C03KZ8SHCTH",  # This is #slackbotr-testing
             text="Hello world! :tada: :left_speech_bubble: :earth_asia:",
         )
     except SlackApiError as e:
-        # You will get a SlackApiError if "ok" is False
-        assert e.response["error"]    # str like 'invalid_auth', 'channel_not_found'
+        # TODO: Abstract away error-handling from the slackbot interface.
+        raise HTTPException(status_code=500, detail=e.response["error"])
