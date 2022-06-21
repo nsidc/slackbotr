3644076cd12ca9385adbd69ab09629b5409a37ca
Robyn Marowitz <robyn.marowitz@colorado.edu>
2022-06-21 22:06:20 +0000

    Add logging for None values to appease typechecker

--- slackbotr/util/import_helpers.py
+++ slackbotr/util/import_helpers.py
@@ -10,6 +10,12 @@ def import_slackbot_endpoints() -> None:
     for module_filepath in slackbot_modules:
         # https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
         spec = importlib.util.spec_from_file_location('*', str(module_filepath))
+        if spec is None or spec.loader is None:
+            raise RuntimeError(f'Failed to import from {module_filepath}')
+
         module = importlib.util.module_from_spec(spec)
+        if module is None:
+            raise RuntimeError(f'Failed to import module from {spec}')
+
         sys.modules[module_filepath.stem] = module
         spec.loader.exec_module(module)
