e962534774f9d9d1f5719b3c8d817498303462c6
Robyn Marowitz <robyn.marowitz@colorado.edu>
2022-06-22 16:31:40 +0000

    Bumpversion config - setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(
+    name='slackbotr',
+    description="Create unique slackbots",
+    version="0.1.0",
+    url="git@github.org:nsidc/slackbotr.git",
+    author="National Snow and Ice Data Center",
+    author_email="nsidc@nsidc.org",
+    packages=['slackbotr'],
+    include_package_data=True,
+    install_requires=[
+        'fastapi',
+    ],
+)
